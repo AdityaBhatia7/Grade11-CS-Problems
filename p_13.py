8c2fe8687717b7492e105dc2997c2e86a3e328d8
AdityaBhatia7 <58880645+AdityaBhatia7@users.noreply.github.com>
2021-12-28 16:15:44 +0530

    Create p_13.py

---
+++ p_13.py
@@ -0,0 +1,29 @@
+print("ADITYA", sep='')
+
+print('A','D','I','T','Y','A', sep=' ', end='')
+
+print('A','D','I','T','Y','A', sep='@', end='')
+
+print('A','D','I','T','Y','A', sep='\t', end='')
+
+print("Aditya"+"\t"+"Bhatia")
+
+print('A','D','I','T','Y','A', sep='\n', end='')
+
+print("\t"*5+'A',"\t"*4+'D',"\t"*3+'I',"\t"*2+'T',"\t"+'Y','A', sep='\n', end='')
+
+print('A',"\t"+'D',"\t"*2+'I',"\t"*3+'T',"\t"*4+'Y',"\t"*5+'A', sep='\n', end='')
+
+print("\u0906"+"\u0926"+"\u0940")
+
+print("\\t","\\b","\\'",'\\"',"\\\\","\\000","\\xhh", sep=' ')
+
+print("the \'purpose\' of \"life\" is a \"life\" of \purpose\ ")
+
+data1 = int(input("enter integer: "))
+data2 = float(input("enter a float: "))
+data3 = bool(input("enter a boolean: "))
+print(type(data1), type(data2), type(data3))
+
+list1 = []
+print(list1)
